004ef06a561eb32447bc4681981d276f79f9f122
Abdullah <145117653+Abdullah0408@users.noreply.github.com>
2024-01-25 01:06:24 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,1 +1,26 @@
-
+//Recursion: Find the 1st & last occurrance of an element in string
+import java.util.*;
+public class Main{
+    public static int first=-1;
+    public static int last=-1;
+    public static void findOccurrance(String str, int idx, char element){
+        if(idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar=str.charAt(idx);
+        if(currChar==element){
+            if(first==-1){
+                first=idx;
+            }else{
+                last=idx;
+            }
+        }
+        findOccurrance(str,idx+1,element);
+    }
+    public static void main(String args[]){
+        String str="abaacdaefaah";
+        findOccurrance(str,0,'a');
+    }
+}
